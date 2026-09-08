b63ef979bc419984e74f6774a60fe6d3e49b2f4e
jgdomingos <jgsdomingoss@gmail.com>
2026-09-08 12:27:46 -0300

    Save Damage-Calculation

---
+++ JS/Damage-calculation/index.js
@@ -0,0 +1,23 @@
+const player1 = prompt("Qual o nome do primeiro personagem?");
+const attack = Number(window.prompt("Qual o poder de ataque do(a) " + player1 + "?"));
+
+const player2 = prompt("Qual o nome do segundo personagem?");
+const life = Number(window.prompt("Quantos pontos de vida o " + player2 + " tem?"));
+const defense = Number(window.prompt("Qual o poder de defesa do " + player2 +"?"));
+const shield = prompt("O personagem " + player2 + " possui Escudo? (S/N)");
+
+let damage = 0;
+
+if (attack > defense && shield === "N") {
+    damage = attack - defense;
+    alert("O dano causado foi de " + damage);
+} else if (attack > defense && shield === "S") {
+    damage = (attack - defense) / 2;
+    alert("O dano causado foi de " + damage);
+} else {
+    alert("O dano causado foi 0");
+}
+
+const updateLife = life - damage;
+
+alert("O personagem " + player2 + " ficou com " + updateLife + " pontos de vida!")

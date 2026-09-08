3bcfe965e7f2f1f135998d1122c89c10c1b31c0f
jgdomingos <jgsdomingoss@gmail.com>
2026-09-08 11:51:45 -0300

    Class if and else

---
+++ JS/IF=Else/index.js
@@ -0,0 +1,19 @@
+if (6 === 6) {
+    console.log("Essa expressão é verdadeira!")
+}
+
+if (7 !== 8) {
+    alert("Essa expressão é verdadeira!")
+} else {
+    alert("Essa expressão não é verdadeira")
+}
+
+const idade = prompt("Informe sua idade:")
+
+if (idade > 18) {
+    alert("Você é maior de idade")
+} else if (idade < 12) {
+    alert("Você ainda é criança")
+} else {
+    alert("Você é menor de idade")
+}

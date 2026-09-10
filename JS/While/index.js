91be1f51b8fd8504829cb4dcdb6b053de092b2af
jgdomingos <jgsdomingoss@gmail.com>
2026-09-10 14:06:17 -0300

    while

--- JS/While/index.js
+++ JS/While/index.js
@@ -4,6 +4,10 @@ while (velocidade > 0) {
     alert("O carro está a " + velocidade + " Km/h")
     velocidade -= 20
     alert("Diminuindo 20 Km/h")
+
+    if (velocidade === 40) {
+        break
+    }
 }
 
 alert("O carro parou!")

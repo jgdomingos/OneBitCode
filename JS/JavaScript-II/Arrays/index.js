7d629b11a82352563203fc28ec4d89f7383efa94
jgdomingos <jgsdomingoss@gmail.com>
2026-09-15 11:25:48 -0300

    Novo modulo de JavaScript (II), aula sobre arrays

---
+++ JS/JavaScript-II/Arrays/index.js
@@ -0,0 +1,9 @@
+const listsDeCompras = ["Açucar", false]
+console.log(listsDeCompras)
+
+listsDeCompras[0] = "Arroz"
+listsDeCompras[1] = "Feijão"
+listsDeCompras[2] = 7
+listsDeCompras[5] = "Batata"
+
+console.log(listsDeCompras)

d8a4ccf54dbff1acd0bba3e47d663991159988bd
jgdomingos <jgsdomingoss@gmail.com>
2026-09-23 14:06:37 -0300

    Funções Anônimas

---
+++ JS/JavaScript-II/Anonymous-Functions/index.js
@@ -0,0 +1,13 @@
+function somar(a, b) {
+    return a + b
+}
+
+const operacao = somar
+
+console.log(operacao(4, 5))
+
+const subtrair = function (a, b) {
+    return a - b
+}
+
+console.log(subtrair(38, 12))
